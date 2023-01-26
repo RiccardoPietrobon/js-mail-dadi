c40dcbe8a505f4c5b94a649651d8f235048b50da
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-26 17:16:50 +0100

    dadi

--- js/dadi.js
+++ js/dadi.js
@@ -1,2 +1,20 @@
 /* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+//random pc
+const randomPCnr = Math.floor(Math.random() * 6) + 1;
+console.log("pc random is ", randomPCnr);
+
+//random user
+const input_dado = document.getElementById("user_dado");
+console.log("user random is ", input_dado);
+
+//button
+const mybutton = document.getElementById("push");
+console.log("buttoooon ", mybutton);
+
+
+mybutton.addEventListener("click", function () {
+    const inserimento = input_dado.value;
+    console.log("mail output ", inserimento);
+})
--- js/mail.js
+++ js/mail.js
@@ -1,5 +1,20 @@
-
 // Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull'esito del controllo.
+
+//mail di input
+const input_mail = document.getElementById("user_mail");
+console.log("mail scritta è ", input_mail);
+
+//button
+const mybutton = document.getElementById("push");
+console.log("buttoooon ", mybutton);
+
+//funzione bottone
+mybutton.addEventListener("click", function () {
+    const inserimento = input_mail.value;
+    console.log("mail output ", inserimento);
+})
+
+
 
 //elenco delle mail
 const mail_list = ["mail1", "mail2", "mail3", "mail4"];
@@ -9,5 +24,14 @@ for (let i = 0; i < mail_list.length; i++) {
 
     const mail_item = mail_list[i];
     console.log("la mail scelta è ", mail_item);
+
+    if (input_mail != mail_item) {
+        console.log("la mail ", input_mail, " non vale")
+    }
+    else (input_mail == mail_item); {
+        console.log("la mail ", input_mail, " vale")
+    }
+
+
 }
 
