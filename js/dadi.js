e65b14a66515c7dc28ab07d252c816267a44802a
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-26 18:39:50 +0100

    if

--- js/dadi.js
+++ js/dadi.js
@@ -1,13 +1,6 @@
 /* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-//random pc
-const randomPCnr = Math.floor(Math.random() * 6) + 1;
-console.log("pc random is ", randomPCnr);
-
-//random user
-const input_dado = document.getElementById("user_dado");
-console.log("user random is ", input_dado);
 
 //button
 const mybutton = document.getElementById("push");
@@ -15,6 +8,36 @@ console.log("buttoooon ", mybutton);
 
 
 mybutton.addEventListener("click", function () {
-    const inserimento = input_dado.value;
-    console.log("mail output ", inserimento);
+
+    //random pc
+    const randomPCnr = Math.floor(Math.random() * 6) + 1;
+    console.log("pc random is ", randomPCnr);
+
+    //random user
+    const input_dado = document.getElementById("user_dado").value;
+    console.log("user random is ", input_dado);
+
+
+
+    for (let i = 0; i > randomPCnr.length; i++) {
+        if (randomPCnr[i] > input_dado) {
+            let victory = 0;
+            console.log("Chi ha vinto?  ", victory);
+
+        }
+        else if (randomPCnr[i] == input_dado) {
+            let victory = 1;
+            console.log("Chi ha vinto?  ", victory);
+
+        }
+
+        else (randomPCnr[i] < input_dado); {
+            let victory = 2;
+            console.log("Chi ha vinto?  ", victory);
+
+        }
+    }
+
+
 })
+
