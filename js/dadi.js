51df67d498f5e1d508e224aa19c26d8b42205f6f
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-27 13:34:36 +0100

    correzione dadi

--- js/dadi.js
+++ js/dadi.js
@@ -1,11 +1,11 @@
 /* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-
 //button
 const mybutton = document.getElementById("push");
 console.log("buttoooon ", mybutton);
 
+const winner = document.getElementById("game_result");
 
 mybutton.addEventListener("click", function () {
 
@@ -14,30 +14,28 @@ mybutton.addEventListener("click", function () {
     console.log("pc random is ", randomPCnr);
 
     //random user
-    const input_dado = document.getElementById("user_dado").value;
+    const input_dado = Math.floor(Math.random() * 6) + 1;
     console.log("user random is ", input_dado);
 
+    let victory;
 
+    if (randomPCnr > input_dado) {
+        victory = "Il PC vale " + randomPCnr + " mentre il giocatore " + input_dado + " quindi vince PC!"
+        console.log("esito ", victory);
 
-    for (let i = 0; i > randomPCnr.length; i++) {
-        if (randomPCnr[i] > input_dado) {
-            let victory = 0;
-            console.log("Chi ha vinto?  ", victory);
+    }
+    else if (randomPCnr < input_dado) {
+        victory = "Il PC vale " + randomPCnr + " mentre il giocatore " + input_dado + " quindi vince giocatore!"
+        console.log("esito ", victory);
 
-        }
-        else if (randomPCnr[i] == input_dado) {
-            let victory = 1;
-            console.log("Chi ha vinto?  ", victory);
+    }
+    else {
+        victory = "Il PC vale " + randomPCnr + " mentre il giocatore " + input_dado + " quindi pareggio"
+        console.log("esito ", victory);
 
-        }
-
-        else (randomPCnr[i] < input_dado); {
-            let victory = 2;
-            console.log("Chi ha vinto?  ", victory);
-
-        }
     }
 
+    winner.innerHTML = victory;
 
 })
 
