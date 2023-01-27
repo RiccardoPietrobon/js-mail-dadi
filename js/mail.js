fa171173f5cf894c460213cf7af6167d11a43b14
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-27 15:11:49 +0100

    correzione mail

--- js/mail.js
+++ js/mail.js
@@ -1,37 +1,46 @@
 // Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull'esito del controllo.
-
-//mail di input
-const input_mail = document.getElementById("user_mail");
-console.log("mail scritta è ", input_mail);
-
 //button
 const mybutton = document.getElementById("push");
 console.log("buttoooon ", mybutton);
 
-//funzione bottone
 mybutton.addEventListener("click", function () {
-    const inserimento = input_mail.value;
-    console.log("mail output ", inserimento);
+
+    //elenco delle mail
+    const mail_list = ["mail1", "mail2", "mail3", "mail4"];
+    console.log("gli utenti sono ", mail_list);
+
+    //inserisci mail
+    const input_mail = document.getElementById("write_mail").value;
+
+    //variabile controllo
+    let check_mail = false;
+
+    for (let i = 0; i < mail_list.length; i++) {
+
+        const mail_item = mail_list[i];
+
+        if (mail_item == input_mail) {
+            check_mail = true;
+
+            console.log(check_mail);
+        }
+    }
+
+    if (check_mail) {
+        //accesso libero
+        console.log("Benvenuyo " + input_mail)
+    }
+
+    else; {
+        //accesso negato
+        console.log("Accesso negato, ritenta con un' altra mail")
+    }
 })
 
 
 
-//elenco delle mail
-const mail_list = ["mail1", "mail2", "mail3", "mail4"];
-console.log("gli utenti sono ", mail_list);
-
-for (let i = 0; i < mail_list.length; i++) {
-
-    const mail_item = mail_list[i];
-    console.log("la mail scelta è ", mail_item);
-
-    if (input_mail != mail_item) {
-        console.log("la mail ", input_mail, " non vale")
-    }
-    else (input_mail == mail_item); {
-        console.log("la mail ", input_mail, " vale")
-    }
 
 
-}
+
+
 
